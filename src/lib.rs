fe32b6ea2f1b7508ddb76ec0716f12357b60aa28
Nikolai Vazquez <nikvzqz@gmail.com>
2020-01-04 02:27:53 -0500

    [Cargo] Create `diffin` crate

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! # Diffin
+//!
+//! A work-in-progress binary diffing tool by [@NikolaiVazquez].
+//!
+//! ## License
+//!
+//! This project is released under either:
+//!
+//! - [MIT License](https://github.com/diffin/diffin/blob/master/LICENSE-MIT)
+//! - [Apache License (Version 2.0)](https://github.com/diffin/diffin/blob/master/LICENSE-APACHE)
+//!
+//! at your choosing.
+//!
+//! [@NikolaiVazquez]: https://twitter.com/nikolaivazquez

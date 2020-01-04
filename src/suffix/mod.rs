a3d678a95b2e54ea82ad107c6934561ba905ef8a
Nikolai Vazquez <nikvzqz@gmail.com>
2020-01-04 14:15:26 -0500

    Add `suffix::iter` convenience function

--- src/suffix/mod.rs
+++ src/suffix/mod.rs
@@ -2,3 +2,10 @@
 
 mod iter;
 pub use iter::SuffixIter;
+
+/// Returns an iterator over the suffixes of a `&str` or `&[T]`, treating it as
+/// the first suffix if non-empty.
+#[inline]
+pub const fn iter<'a, S: ?Sized>(s: &'a S) -> SuffixIter<'a, S> {
+    SuffixIter::new(s)
+}
